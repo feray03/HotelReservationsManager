9b83de0a4cfd4cbdb6990b5a9ab0c873b40afc16
feray03 <feray_03fahri@abv.bg>
2022-03-27 11:04:32 +0300

    class Reservation - edited

--- HotelReservationsManager/Data/Entities/Reservation.cs
+++ HotelReservationsManager/Data/Entities/Reservation.cs
@@ -4,21 +4,21 @@ using System.Text;
 
 namespace Data.Entities
 {
-     public class Reservation
+     public class Reservation : BaseEntity
     {
         public Reservation(Reservation reservation)
         {
-            this.Id = resetrvation.Id;
-            this.Room = new Room(reservation.Room);
-            this.User = new User(reservation.User);
-            this.CheckInDate = reservation.CheckInDate;
-            this.IsAllInclusive = reservation.IsAllInclusive;
-            this.FinalPrice = reservation.FinalPrice;
-            this.ClientReservations = reservation.ClientReservations;
+            Id = reservation.Id;
+            Room = new Room(reservation.Room);
+            User = new User(reservation.User);
+            CheckInDate = reservation.CheckInDate;
+            IsAllInclusive = reservation.IsAllInclusive;
+            FinalPrice = reservation.FinalPrice;
+            ClientReservations = reservation.ClientReservations;
         }
         public Reservation()
         {
-            ClientReservations = new HashSet<ClientReservarvation>();
+            ClientReservations = new HashSet<ClientReservation>();
         }
         public int RoomId { get; set; }
         public virtual Room Room { get; set; }
@@ -27,6 +27,8 @@ namespace Data.Entities
         public DateTime CheckInDate { get; set; }
         public DateTime CheckOutDate { get; set; }
         public bool IncludedBreakfast { get; set; }
+        public bool IsAllInclusive { get; set; }
         public Double FinalPrice { get; set; }
+        public ICollection<ClientReservation> ClientReservations { get; set; }
     }
 }
