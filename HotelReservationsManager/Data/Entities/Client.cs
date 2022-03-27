fee002460e90b44227da63bf8ed6bf921063a4fb
feray03 <feray_03fahri@abv.bg>
2022-03-27 10:21:01 +0300

    class Client - finish

--- HotelReservationsManager/Data/Entities/Client.cs
+++ HotelReservationsManager/Data/Entities/Client.cs
@@ -1,10 +1,43 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Text;
 
 namespace Data.Entities
 {
-    class Client
+    public class Client :BaseEntity
     {
+        public Client()
+        {
+
+        }
+
+        public Client(Client client)
+        {
+            Id = client.Id;
+            FirstName = client.FirstName;
+            LastName = client.LastName;
+            PhoneNumber = client.PhoneNumber;
+            Email = client.Email;
+            IsAdult = client.IsAdult;
+            ClientReservations = client.ClientReservations;
+        }
+
+        [StringLength(50, MinimumLength = 3)]
+        [Required]
+        public string FirstName { get; set; }
+
+        [StringLength(50, MinimumLength = 3)]
+        [Required]
+        public string LastName { get; set; }
+
+        [StringLength(10, MinimumLength = 10)]
+        [Required]
+        public string PhoneNumber { get; set; }
+
+        [Required]
+        public string Email { get; set; }
+        public bool IsAdult { get; set; }
+        public virtual ICollection<ClientReservation> ClientReservations { get; set; }
     }
 }
---
+++ HotelReservationsManager/Data/Entities/ClientReservation.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Data.Entities
+{
+    public class ClientReservation
+    {
+        public ClientReservation()
+        {
+
+        }
+
+        public ClientReservation(int clientId, Client client, int reservationId, Reservation reservation)
+        {
+            ClientId = clientId;
+            Client = client;
+            ReservationId = reservationId;
+            Reservation = reservation;
+        }
+        public int Id { get; set; }
+        public int ClientId { get; set; }
+        public virtual Client Client { get; set; }
+        public int ReservationId { get; set; }
+        public virtual Reservation Reservation { get; set; }
+    }
+}
