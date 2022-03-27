f609ef738a00df66f19b8bf936c6eee84d7e4e7c
Моника Джелепова <monikadzhelepova03@pmg-haskovo.org>
2022-03-27 10:58:44 +0300

    Reservation

--- HotelReservationsManager/Data/Entities/Reservation.cs
+++ HotelReservationsManager/Data/Entities/Reservation.cs
@@ -6,6 +6,27 @@ namespace Data.Entities
 {
      public class Reservation
     {
-
+        public Reservation(Reservation reservation)
+        {
+            this.Id = resetrvation.Id;
+            this.Room = new Room(reservation.Room);
+            this.User = new User(reservation.User);
+            this.CheckInDate = reservation.CheckInDate;
+            this.IsAllInclusive = reservation.IsAllInclusive;
+            this.FinalPrice = reservation.FinalPrice;
+            this.ClientReservations = reservation.ClientReservations;
+        }
+        public Reservation()
+        {
+            ClientReservations = new HashSet<ClientReservarvation>();
+        }
+        public int RoomId { get; set; }
+        public virtual Room Room { get; set; }
+        public string UserId { get; set; }
+        public virtual User User { get; set; }
+        public DateTime CheckInDate { get; set; }
+        public DateTime CheckOutDate { get; set; }
+        public bool IncludedBreakfast { get; set; }
+        public Double FinalPrice { get; set; }
     }
 }
