b6f652665af50b7062ae2e073ec0c48d0c5cefca
feray03 <feray_03fahri@abv.bg>
2022-03-27 09:35:15 +0300

    .

---
+++ HotelReservationsManager/Data/Entities/RoomTypeEnum.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Data.Entities
+{
+    public enum RoomTypeEnum
+    {
+        TwoBeds,
+        Apartament,
+        DubleBad,
+        Penthouse,
+        Maisonette
+    }
+}
