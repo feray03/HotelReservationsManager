fffb19be2c145ec7c5c16911679abea018bdced1
feray03 <feray_03fahri@abv.bg>
2022-04-02 14:59:09 +0300

    edited

--- HotelReservationsManager/Web/Models/Rooms/RoomsCreateViewModel.cs
+++ HotelReservationsManager/Web/Models/Rooms/RoomsCreateViewModel.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Data.Entities;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
 using System.Linq;
@@ -12,8 +13,8 @@ namespace Web.Models.Rooms
         [Range (1,int.MaxValue,ErrorMessage = "Negative values or 0 are not accepted")]
         public int Capasity { get; set; }
         [Required]
-        [EnumDataType(typeof(TypeRoom))]
-        public TypeRoom type { get; set; }
+        [EnumDataType(typeof(RoomTypeEnum))]
+        public RoomTypeEnum type { get; set; }
         [Required]
         public bool IsFree { get; set; }
         [Required]
@@ -23,7 +24,7 @@ namespace Web.Models.Rooms
         [Range(0, double.MaxValue, ErrorMessage = "Negative values are not accepted")]
         public decimal PriceForKid { get; set; }
         [Required]
-        [StringLenght(6, MinimumLenght = 2 , ErrorMessage = "Must be at least 2 characters long. Cannot be more than 6 characters")];
+        [StringLength(6, MinimumLength = 2 , ErrorMessage = "Must be at least 2 characters long. Cannot be more than 6 characters")]
         public string number { get; set; }
 
     }
