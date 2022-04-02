1ca116c380d3edf330aaf1a7d79b3edff51c9015
feray03 <feray_03fahri@abv.bg>
2022-04-02 12:00:17 +0300

    class UsersController - 1

--- HotelReservationsManager/Web/Controllers/UsersController.cs
+++ HotelReservationsManager/Web/Controllers/UsersController.cs
@@ -12,6 +12,97 @@ namespace Web.Controllers
 {
     public class UsersController : Controller
     {
-        
+        private readonly HotelDbContext _context;
+
+        public UsersController(HotelDbContext context)
+        {
+            _context = context;
+        }
+
+        // GET: Users
+        public async Task<IActionResult> Index()
+        {
+            return View(await _context.Users.ToListAsync());
+        }
+
+        // GET: EditUsers
+        public async Task<IActionResult> EditUsers()
+        {
+            return View(await _context.Users.ToListAsync());
+        }
+
+        // GET: DeleteUsers
+        public async Task<IActionResult> DeleteUsers()
+        {
+            return View(await _context.Users.ToListAsync());
+        }
+
+        // GET: LockUsers
+        public async Task<IActionResult> LockUsers()
+        {
+            return View(await _context.Users.ToListAsync());
+        }
+
+        // GET: Users/Details/5
+        public async Task<IActionResult> Details(string id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var user = await _context.Users.FirstOrDefaultAsync(m => m.Id == id);
+            if (user == null)
+            {
+                return NotFound();
+            }
+
+            return View(user);
+        }
+
+        // GET: Users/Create
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Users/Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("FirstName,MiddleName,LastName,EGN,HireDate,IsActive,IsAdmin,FireDate,Id,UserName,NormalizedUserName,Email,NormalizedEmail,EmailConfirmed,PasswordHash,SecurityStamp,ConcurrencyStamp,PhoneNumber,PhoneNumberConfirmed,TwoFactorEnabled,LockoutEnd,LockoutEnabled,AccessFailedCount")] User user)
+        {
+            if (ModelState.IsValid)
+            {
+                user.Id = Guid.NewGuid().ToString();
+                _context.Add(user);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            return View(user);
+        }
+
+        // GET: Users/Edit/5
+        public async Task<IActionResult> Edit(string id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var user = await _context.Users.FindAsync(id);
+            if (user == null)
+            {
+                return NotFound();
+            }
+            return View(user);
+        }
+
+        // POST: Users/Edit/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(string id, [Bind("FirstName,MiddleName,LastName,EGN,HireDate,IsActive,IsAdmin,FireDate,Id,UserName,NormalizedUserName,Email,NormalizedEmail,EmailConfirmed,PasswordHash,SecurityStamp,ConcurrencyStamp,PhoneNumber,PhoneNumberConfirmed,TwoFactorEnabled,LockoutEnd,LockoutEnabled,AccessFailedCount")] User user)
+        {
+
+        }
     }
 }
