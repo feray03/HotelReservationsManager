36e6c22e0b1dcff365488b295b5debd93e3228a7
feray03 <feray_03fahri@abv.bg>
2022-04-02 13:58:50 +0300

    Model - edit

---
+++ HotelReservationsManager/Web/Models/PagerViewModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Web.Models
+{
+    public class PagerViewModel
+    {
+        public int CurrentPage { get; set; }
+
+        public int PagesCount { get; set; }
+    }
+}
