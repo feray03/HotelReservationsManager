3de2863618d0df24a0ab097d08ec3603def23211
feray03 <feray_03fahri@abv.bg>
2022-04-03 10:27:15 +0300

    reservations-model

--- HotelReservationsManager/Web/Models/Reservations/ReservationsViewModel.cs
+++ HotelReservationsManager/Web/Models/Reservations/ReservationsViewModel.cs
@@ -1,5 +1,7 @@
-﻿using System;
+﻿using Data.Entities;
+using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -7,5 +9,18 @@ namespace Web.Models.Reservations
 {
     public class ReservationsViewModel
     {
+        public string Id { get; set; }
+        public string RoomNumber { get; set; }
+        public string UserName { get; set; }
+        public ICollection<ClientReservation> Clients { get; set; }
+
+        [DataType(DataType.Date)]
+        public DateTime CheckInDate { get; set; }
+
+        [DataType(DataType.Date)]
+        public DateTime CheckOutDate { get; set; }
+        public string IncludedBreakfast { get; set; }
+        public string IsAllInclusive { get; set; }
+        public decimal FinalPrice { get; set; }
     }
 }
